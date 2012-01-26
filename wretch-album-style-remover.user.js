0f722f093334a532725c6003b0821e5694b69b2e
Gea-Suan Lin <darkkiller@gmail.com>
2012-01-26 14:48:30 +0800

    Using obj.length directly

--- wretch-album-style-remover.user.js
+++ wretch-album-style-remover.user.js
@@ -14,8 +14,7 @@
     }
 
     obj = document.getElementsByTagName('*');
-    l = obj.length;
-    for (var i = 0; i < l; i++) {
+    for (var i = 0; i < obj.length; i++) {
         obj[i].style.cssText = '';
     }
 
