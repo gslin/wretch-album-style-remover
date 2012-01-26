43e8675428f902bc4f7f5dd0b9ad63aaf3e7794f
Gea-Suan Lin <darkkiller@gmail.com>
2012-01-26 14:44:10 +0800

    init

---
+++ wretch-album-style-remover.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        Wretch Album Style Remover
+// @version     2011.1125.03
+// @namespace   http://blog.gslin.org/plugins/wretch-album-style-remover
+// @description Remove wretch album style
+// @homepage    http://blog.gslin.org/plugins/wretch-album-style-remover
+// @include     http://www.wretch.cc/album/*
+// ==/UserScript==
+
+(function(){
+    var obj = document.styleSheets;
+    for (var i = 0; i < obj.length; i++) {
+        obj.item(i).disabled = true;
+    }
+
+    obj = document.getElementsByTagName('*');
+    l = obj.length;
+    for (var i = 0; i < l; i++) {
+        obj[i].style.cssText = '';
+    }
+
+    var kukubar_upper = document.getElementById('kukubar-upper');
+    kukubar_upper.style.display = 'none';
+
+    var kukubar_lower = document.getElementById('kukubar-lower');
+    kukubar_lower.style.display = 'none';
+})();
